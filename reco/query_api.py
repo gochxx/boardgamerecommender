ceda3e37da6947feac46ff05b1a722e220a3fa13
gochxx <falko.trischler@web.de>
2024-03-08 08:40:35 +0100

    inital setup of bgreco.html

--- reco/query_api.py
+++ reco/query_api.py
@@ -3,12 +3,7 @@ import json
 
 
 url = "http://192.168.178.26:5000/predict"  # Stellen Sie sicher, dass die Adresse und der Port korrekt sind
-data = {
-    "feature1": 5.1,
-    "feature2": 3.5,
-    "feature3": 8.4,
-    "feature4": 5.2
-}
+
 data = {"yearpublished": 2020, "playingtime": 60, "age": 10, 
               "cat": ["cat_CardGame", "cat_ScienceFiction", "cat_Dice", "cat_Animals"], 
               "mec": ["mec_DiceRolling", "mec_ModularBoard"]}
